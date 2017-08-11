b06fd4a9ab3eb7eae7eb319fe44f95a7579ea193
yinsujun <ysj@in-next.cn>
2017-08-11 15:00:45 +0800

    弧形进度条

--- app/src/main/java/com/progressapplication/MainActivity.java
+++ app/src/main/java/com/progressapplication/MainActivity.java
@@ -4,15 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.mylibrary.CircleProgressView;
+import com.mylibrary.ColorArcProgressBar;
 
 public class MainActivity extends AppCompatActivity {
     private CircleProgressView circleProgressView;
+    private ColorArcProgressBar colorArcProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_circle_progress_view);
         initView();
+        initColorProgress();
     }
     private void initView(){
         circleProgressView= (CircleProgressView) findViewById(R.id.progress_view);
@@ -22,5 +25,9 @@ public class MainActivity extends AppCompatActivity {
         circleProgressView.setBottomText("下方文字");
         circleProgressView.setProgress(80);
         circleProgressView.setSpeed(40);
+    }
+    private void initColorProgress(){
+        colorArcProgressBar= (ColorArcProgressBar) findViewById(R.id.color_progress_bar);
+        colorArcProgressBar.setCurrentValues(80);
     }
 }
